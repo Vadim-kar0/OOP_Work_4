483cff20bbd7f379694385b829a67c17b4ef9497
вадим <funpalka1@gmail.com>
2023-09-05 22:39:40 +0400

    Аштфд

---
+++ src/main/java/Task2/StudentView.java
@@ -0,0 +1,19 @@
+package Task2;
+
+import org.example.Student;
+
+import java.util.List;
+import java.util.logging.Logger;
+
+
+public class StudentView<T extends Student> implements UserView<T> {
+
+    public void sendOnConsole(List<T> students) {
+        for(T user: students){
+            System.out.println(user);
+        }
+    }
+
+
+
+}
